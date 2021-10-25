77d230b4a3f1d685734e209bc07da3c9b939373c
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-10-25 20:58:36 +0300

    Create slider component

---
+++ src/Components/SliderPage/Slider/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Slider() {
+    return (
+        <>
+        <div>
+            <button></button>
+            <input type="text" />
+            <button></button>
+        </div>
+        <div>
+            <button></button>
+            <div></div>
+            <button></button>
+        </div>
+        </>
+    )
+}
