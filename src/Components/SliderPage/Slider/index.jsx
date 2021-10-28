9b5658ac58a2002369d6942a73d0df3110c13e3b
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-10-28 20:15:26 +0300

    Update slider .jsx file

--- src/Components/SliderPage/Slider/index.jsx
+++ src/Components/SliderPage/Slider/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import styles from './slider.module.sass'
 
-export default function Slider() {
+export default function Slider(props) {
     return (
         <>
-        <div>
-            <button></button>
+        <div className={styles.settingsPanelWrapper}>
+            <button>Slide show</button>
+            <div className={styles.inputWrapper}>
+            <p>Choose delay slide show in second</p>
             <input type="text" />
-            <button></button>
+            </div>
+            <button>Full screen</button>
         </div>
-        <div>
-            <button></button>
-            <div></div>
-            <button></button>
+        <div className={styles.sliderWrapper}>
+            <button>Prev</button>
+            <img className={styles.image} src={props.slides[4]} alt="" />
+            <button>Next</button>
         </div>
         </>
     )
