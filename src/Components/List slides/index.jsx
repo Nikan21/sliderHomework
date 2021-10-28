a50a518a9c6c0da9d0f51b08b3455cf5aade641a
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-10-28 20:10:44 +0300

    Add array list images

--- src/Components/List slides/index.jsx
+++ src/Components/List slides/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const slides = [];
+export const slides = ['/images/Iron_Man.png', '/images/Captain_America.png', '/images/Thor.png', '/images/Hulk.png', '/images/Hawkeye.png', '/images/Black_Widow.png'];
 
 export default function ListSlides() {
     return (
