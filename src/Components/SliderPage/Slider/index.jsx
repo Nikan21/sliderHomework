3098084f17cc730721eed274842620e2de46a482
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-11-01 14:39:39 +0200

    Add not working code

--- src/Components/SliderPage/Slider/index.jsx
+++ src/Components/SliderPage/Slider/index.jsx
@@ -3,6 +3,10 @@ import styles from './slider.module.sass'
 
 export default function Slider(props) {
     let [click, setClick] = useState(0)
+    
+    let [delay, setDelay] = useState(0)
+
+    let [autoClick, changeAutoClick] = useState(null);
 
     const clickPrevSlide = ()=>{
         setClick(click = (click - 1 + props.slides.length) % props.slides.length)
@@ -12,19 +16,39 @@ export default function Slider(props) {
         setClick(click = (click + 1) % props.slides.length)
     }
 
-    const slideShow = ()=>{
-        setInterval(clickNextSlide, 3000)
+    const handleChange = (event)=>{
+        let{
+            target: {value: delay},
+        } = event;
+
+        setDelay(Number(delay*1000))
+    }
+    
+    const startSlideShow = ()=>{
+        if (autoClick === null){
+            autoClick = setInterval(clickNextSlide, delay)
+        }
     }
 
-    console.log(props.slides)
-    console.log(click)
+    /*Вопрос*/
+    
+    const stopSlideShow = ()=>{
+        clearInterval(autoClick);
+        changeAutoClick(null);
+    }
+    
+    console.log(stopSlideShow())
+    console.log(delay)
     return (
         <>
         <div className={styles.settingsPanelWrapper}>
-            <button onClick={slideShow}>Slide show</button>
+            <div className={styles.buttonsSlideShowWrapper}>
+            <button className={styles.ButtonSlideShow} onClick={startSlideShow}>Slide show</button>   
+            <button className={styles.ButtonSlideShow} onClick={stopSlideShow}>Stop slide show</button>
+            </div>
             <div className={styles.inputWrapper}>
             <p>Choose delay slide show in second</p>
-            <input type="text" />
+            <input type="text" name='delay' value={delay} onChange={handleChange}/>
             </div>
             <button>Full screen</button>
         </div>
